e8d079c83ca493a56d5df2dd8d358f1bafacf648
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-12-16 23:33:59 +0545

    feat:update image position logic and update ui

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -16,6 +16,8 @@ const Puzzle: React.FC = () => {
     handleMove,
     resetPuzzle,
     feedback,
+    incorrectMoves,
+    failure,
   } = usePuzzleData();
 
   //for drag and drop the pieces
@@ -33,31 +35,44 @@ const Puzzle: React.FC = () => {
     event.preventDefault();
   };
 
-  // to calculate background position for each piece
   const getBackgroundPosition = (index: number, size: number) => {
     const row = Math.floor(index / size);
     const col = index % size;
-    return `${(col * 100) / (size - 1)}% ${(row * 100) / (size - 1)}%`;
+
+    const xPosition = (col * 100) / (size - 1);
+    const yPosition = (row * 100) / (size - 1);
+
+    return `${xPosition}% ${yPosition}%`;
   };
 
-  return (
-    <div className="w-full min-h-screen  flex flex-col md:flex-row  items-center justify-center bg-gradient-to-r from-blue-900 to-cyan-800 px-6 py-10">
-      <div className="md:w-10/12 flex flex-col items-center justify-center  ">
-        <h1 className="text-2xl font-bold text-white mb-4">
-          Dynamic Puzzle Game
-        </h1>
+  // useEffect(() => {
+  //   console.log(getBackgroundPosition(0, 2));  // 0% 0%
+  //   console.log(getBackgroundPosition(1, 2)); // 100% 0%
+  //   console.log(getBackgroundPosition(2, 2)); // 0% 100%
+  //   console.log(getBackgroundPosition(3, 2));  // 100% 100%
+  // }, []);
 
-        <div className="w-5/12 mb-4 flex justify-between ">
-          {/* Grid size selector */}
-          <div className="flex">
-            <label htmlFor="gridSize" className="mr-2 text-white">
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-900 to-cyan-800 px-6 py-10">
+      <h1 className="text-4xl font-bold text-white/80 mb-10">
+        Dynamic Puzzle Game
+      </h1>
+      <div className="w-full flex justify-between gap-4 items-center">
+        {/* Insights Area */}
+        <div className="w-3/12 h-full flex flex-col justify-between gap-y-6 items-center px-6 font-semibold text-white">
+          {/* Grid Size Selector */}
+          <div className="flex items-center mb-8">
+            <label
+              htmlFor="gridSize"
+              className="mr-4 text-xl font-medium text-white/80"
+            >
               Select Grid Size:
             </label>
             <select
               id="gridSize"
               value={gridSize}
               onChange={(e) => setGridSize(Number(e.target.value))}
-              className="border rounded px-2 bg-gray-800 text-white"
+              className="border-2 border-white/80 rounded-lg px-4 py-2 bg-gray-800 text-white/90 focus:outline-none cursor-pointer hover:scale-105"
             >
               {[...Array(11).keys()].map((_, i) => (
                 <option key={i + 2} value={i + 2}>
@@ -67,59 +82,99 @@ const Puzzle: React.FC = () => {
             </select>
           </div>
 
-          {/* level timer and score */}
-          <div className="flex gap-x-4 font-semibold text">
-            <h2>Level: {level}</h2>
-            <h2>Score: {score}</h2>
-            <h2>Time Left: {timer}s</h2>
+          {/* Level, Score, Timer */}
+          <div className="flex flex-col text-lg gap-y-2 text-white/80 mb-8">
+            <h2>
+              Level: <span className="text-black font-semibold"> {level}</span>
+            </h2>
+            <h2>
+              Score: <span className="text-black font-semibold"> {score}</span>
+            </h2>
+            <h2>
+              Time Left:
+              <span className="text-black font-semibold"> {timer} s</span>
+            </h2>
+          </div>
+
+          {/* Incorrect Moves & Failures */}
+          <div className="flex flex-col text-lg gap-y-2 text-white/80">
+            <h2>
+              Incorrect Moves:
+              <span className="text-black font-semibold">
+                {" "}
+                {incorrectMoves}
+              </span>
+            </h2>
+            <h2>
+              Failures:
+              <span className="text-black font-semibold"> {failure}</span>
+            </h2>
           </div>
         </div>
 
-        {/* Puzzle grid */}
-        <div
-          className="grid gap-"
-          style={{
-            gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
-            gridTemplateRows: `repeat(${gridSize}, 1fr)`,
-            width: "500px",
-            height: "500px",
-          }}
-        >
-          {shuffledPieces.map((piece, index) => (
+        {/* Puzzle Grid */}
+        <div className="w-6/12 flex flex-col justify-center items-center">
+          <div
+            className="grid gap-0.5"
+            style={{
+              gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+              gridTemplateRows: `repeat(${gridSize}, 1fr)`,
+              width: "500px",
+              height: "500px",
+            }}
+          >
+            {shuffledPieces.map((piece, index) => (
+              <div
+                key={index}
+                draggable
+                onDragStart={(e) => handleDragStart(index, e)}
+                onDrop={(e) => handleDrop(index, e)}
+                onDragOver={handleDragOver}
+                className={`relative border-2 ${
+                  correctPositions[index] === piece
+                    ? "border-1 border-green-500"
+                    : "border-2 border-red-500"
+                } rounded-md shadow-md`}
+                style={{
+                  width: `${500 / gridSize}px`,
+                  height: `${500 / gridSize}px`,
+                  backgroundImage: `url(${image})`,
+
+                  backgroundPosition: getBackgroundPosition(
+                    piece - 1,
+                    gridSize
+                  ),
+
+                  backgroundSize: `${gridSize * 100}%`,
+                }}
+              >
+                {piece}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Feedback Section */}
+        <div className="w-3/12 h-full flex flex-col items-center">
+          <h1 className="text-white/80 text-2xl font-semibold underline mb-6">
+            Feedback:
+          </h1>
+          {/* If game complete */}
+          {isSolved() && shuffledPieces.length > 0 && (
+            <div className="mt-4 p-4 bg-green-600 text-white font-bold rounded-xl shadow-lg transition transform duration-300 ease-in-out hover:scale-105">
+              Puzzle Completed!
+            </div>
+          )}
+
+          {feedback && (
             <div
-              key={index}
-              draggable
-              onDragStart={(e) => handleDragStart(index, e)}
-              onDrop={(e) => handleDrop(index, e)}
-              onDragOver={handleDragOver}
-              className={`border ${
-                correctPositions[index] === piece
-                  ? "border-2 border-green-500"
-                  : " border-2 border-red-500"
-              }`}
-              style={{
-                width: `${500 / gridSize}px`,
-                height: `${500 / gridSize}px`,
-                backgroundImage: `url(${image})`,
-                backgroundPosition: getBackgroundPosition(piece, gridSize),
-                backgroundSize: `${gridSize * 100}%`,
-              }}
-            ></div>
-          ))}
+              className="mt-4 p-4  text-white font-bold rounded-xl shadow-lg cursor-pointer transition transform duration-300 ease-in-out hover:scale-105"
+              onClick={resetPuzzle}
+            >
+              {feedback}
+            </div>
+          )}
         </div>
-
-        {/* if game complete  */}
-        {isSolved() && (shuffledPieces.length > 0 ) && (
-          <div className="mt-4 p-2 bg-green-600 text-white font-bold rounded">
-            Puzzle Completed!
-          </div>
-        )}
-
-        {feedback && (
-          <div className="mt-4 p-2 bg-green-600 text-white font-bold rounded cursor-pointer" onClick={resetPuzzle}>
-            {feedback}
-          </div>
-        )}
       </div>
     </div>
   );
--- src/context/PuzzleContext.tsx
+++ src/context/PuzzleContext.tsx
@@ -44,36 +44,6 @@ export const PuzzleProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const timerRef = useRef<NodeJS.Timeout | null>(null);
 
-  // //to generate the required pieces
-  // const generatePieces = (size: number) =>
-  //   Array.from({ length: size * size }, (_, i) => i + 1);
-
-  // //to shuffle the pieces randomly
-  // const shufflePieces = (pieces: number[]) => {
-  //   let shuffled = [...pieces];
-  //   do {
-  //     shuffled = shuffled.sort(() => Math.random() - 0.5);
-  //   } while (isSolvable(shuffled, gridSize));
-
-  //   return shuffled;
-  // };
-
-  // // to check if the puzzle is solvable
-  // const isSolvable = (pieces: number[], size: number) => {
-  //   let inversions = 0;
-  //   for (let i = 0; i < pieces.length; i++) {
-  //     for (let j = i + 1; j < pieces.length; j++) {
-  //       if (pieces[i] > pieces[j] && pieces[i] !== 0 && pieces[j] !== 0)
-  //         inversions++;
-  //     }
-  //   }
-  //   if (size % 2 === 0) {
-  //     const rowFromBottom = Math.floor(pieces.indexOf(0) / size) + 1;
-  //     return (rowFromBottom % 2 === 0) === (inversions % 2 === 0);
-  //   }
-  //   return inversions % 2 === 0;
-  // };
-
   const resetPuzzle = () => {
     const pieces = generatePieces(gridSize);
     setCorrectPositions(pieces);
@@ -277,6 +247,7 @@ export const PuzzleProvider: React.FC<{ children: React.ReactNode }> = ({
     if (level <= 10) {
       if (failure >= 3) {
         setTimeout(() => {
+          setFailure(0);
           startAgain();
         }, 3000);
       }
