339d35c580db50def14b2788d075f9ba5eeaad3d
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-12-17 13:47:44 +0545

    mf

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, } from "react";
 import { usePuzzleData } from "../context/PuzzleContext";
 
 const Puzzle: React.FC = () => {
